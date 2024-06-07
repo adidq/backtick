bb1e355552d437010ed4754a5f56b7da23ee8de0
adidq <codesep8@gmail.com>
2024-06-07 11:49:54 +0900

    update

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -1,8 +1,7 @@
-import { ThemeToggle } from "./themetoggle";
-
 export default function Footer() {
     return (
         <footer>
+            <hr/>
             <p>footer</p>
         </footer>
     )
