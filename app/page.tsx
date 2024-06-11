1ee8017a68a2469e66d66903a59141128884264b
adidq <codesep8@gmail.com>
2024-06-12 01:06:23 +0900

    re-init

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
-import { AdCard } from "@/components/cards/adcard";
-import { ContentCard } from "@/components/cards/contentcard";
-
 export default function Home() {
   return (
-    <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-      <ContentCard/>
-      <ContentCard/>
-      <ContentCard/>
-      <AdCard/>
-      <ContentCard/>
-      <ContentCard/>
-      <AdCard/>
-      <ContentCard/>
-    </div>
-  )
+    <main>
+      <p>hi</p>
+    </main>
+  );
 }
